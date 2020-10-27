61285739988255d8492a1f8987f6e57336e6cb64
JennyferChang <jenniferchang94@gmail.com>
2020-10-26 22:07:13 -0700

    Updates

--- app.js
+++ app.js
@@ -1,89 +1,51 @@
+// const createError = require("http-errors");
 const express = require("express");
-const axios = require("axios");
-const cheerio = require("cheerio");
-const PORT = process.env.PORT || 9000;
+const path = require("path");
+const cookieParser = require("cookie-parser");
+//var logger = require("morgan");
 
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(express.static("public"));
+//const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}.`);
-});
+// view engine setup
+// app.engine("pug", require("pug").__express);
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "pug");
+//
+//
+//
+// app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-  res.sendFile("reactapp/public/index.html");
-});
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
-let ids = [
-  "16078355",
-  "16078363",
-  "16077601",
-  "16157843",
-  "15996385",
-  "15996388",
-  "15843625",
-  "15843829",
-  "15989183",
-  "15989245",
-  "15889485",
-  "16155165",
-  "16113242",
-  "15935798",
-  "15840526",
-  "15992988",
-  "16118066",
-  "16039442",
-  "15840509",
-];
+// // catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
+//
+// // error handler
+// app.use(function (err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get("env") === "development" ? err : {};
+//
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render("error");
+// });
+//
+//
+// // app.listen(PORT, () => {
+// //   console.log(`App listening on port ${PORT}.`);
+// // });
 
-let names = [];
-let genders = [];
-let breeds = [];
-let ages = [];
-let links = [];
-let picLinks = [];
-
-ids.forEach(scraperFunction);
-function scraperFunction(item, index) {
-  axios
-    .get(`https://www.ilovefamilydog.org/dog-details/?id=${item}`)
-    .then((response) => {
-      let $ = cheerio.load(response.data);
-
-      names[index] = `${$("h1").text()}`;
-      genders[index] = $("td")
-        .filter(function () {
-          return $(this).text().trim() === "GENDER:";
-        })
-        .next()
-        .text();
-      breeds[index] = $("td")
-        .filter(function () {
-          return $(this).text().trim() === "PRIMARY BREED:";
-        })
-        .next()
-        .text();
-      ages[index] = $("td")
-        .filter(function () {
-          return $(this).text().trim() === "AGE:";
-        })
-        .next()
-        .text()
-        .trim();
-
-      picLinks[index] = `${$(".img").attr("href")}`;
-
-      links[index] = `https://www.ilovefamilydog.org/dog-details/?id=${item}`;
-
-      console.log(genders);
-      console.log(breeds);
-      console.log(ages);
-      console.log(names);
-      console.log(picLinks);
-      console.log(links);
-    });
-}
+module.exports = app;
