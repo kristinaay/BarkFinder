7f6dcbf13a0dbd64a50410f704fd2e2e833c8c2c
JennyferChang <jenniferchang94@gmail.com>
2020-10-28 07:57:40 -0700

    Updated path to build to connect frontend + backend

---
+++ app.js
@@ -0,0 +1,51 @@
+// const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+//const cookieParser = require("cookie-parser");
+//var logger = require("morgan");
+
+const indexRouter = require("./myapp/routes");
+const usersRouter = require("./myapp/routes/users");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+//app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "reactapp/build")));
+
+//const PORT = process.env.PORT || 3001;
+
+// view engine setup
+// app.engine("pug", require("pug").__express);
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "pug");
+//
+//
+//
+// app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+// // catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
+//
+// // error handler
+// app.use(function (err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get("env") === "development" ? err : {};
+//
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render("error");
+// });
+//
+//
+// // app.listen(PORT, () => {
+// //   console.log(`App listening on port ${PORT}.`);
+// // });
+
+module.exports = app;
