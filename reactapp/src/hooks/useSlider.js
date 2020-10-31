0a9d8eaca64bb8ac130efa9514c976d98f8de16e
JennyferChang <jenniferchang94@gmail.com>
2020-10-30 20:23:07 -0700

    Updating slider for slideshow

--- reactapp/src/hooks/useSlider.js
+++ reactapp/src/hooks/useSlider.js
@@ -1,64 +1,64 @@
-import { useEffect } from 'react'
+import { useEffect } from "react";
 
 const useSlider = (slideImage, slideText, images) => {
-    let slideCounter = 0;
+  let slideCounter = 0;
 
-    useEffect(() => startSlider())
+  useEffect(() => startSlider(), [images]); //fix
 
-    const startSlider =() => {
-        slideImage.current.style.backgroundImage = `linear-gradient(
+  const startSlider = () => {
+    slideImage.current.style.backgroundImage = `linear-gradient(
             to right,
             rgba(34, 34, 34, 0.4),
             rgba(68, 68, 68, 0.4)
-            ), url(${images[0].src})`;
-        slideText.current.innerHTML = images[0].text;
-    }
+            ), url(${images[0].picLink})`;
+    slideText.current.innerHTML = images[0].name;
+  };
 
-    const handleSlide = (slide) => {
-        slideImage.current.style.backgroundImage = `linear-gradient(
+  const handleSlide = (slide) => {
+    slideImage.current.style.backgroundImage = `linear-gradient(
             to right,
             rgba(34, 34, 34, 0.4),
             rgba(68, 68, 68, 0.4)
-          ), url(${images[slide - 1].src})`;
-        slideText.current.innerHTML = images[slide - 1].text;
-        animateSlide(slideImage)
+          ), url(${images[slide - 1].picLink})`;
+    slideText.current.innerHTML = images[slide - 1].name;
+    animateSlide(slideImage);
+  };
+
+  const animateSlide = () => {
+    slideImage.current.classList.add("fadeIn");
+    setTimeout(() => {
+      slideImage.current.classList.remove("fadeIn");
+    }, 700);
+  };
+
+  const goToPreviousSlide = () => {
+    if (slideCounter === 0) {
+      handleSlide(images.length);
+      slideCounter = images.length;
     }
 
-    const animateSlide = () => {
-        slideImage.current.classList.add('fadeIn');
-        setTimeout(() => {
-            slideImage.current.classList.remove('fadeIn');
-        }, 700);
+    handleSlide(slideCounter);
+    slideCounter--;
+  };
+
+  const goToNextSlide = () => {
+    if (slideCounter === images.length - 1) {
+      startSlider();
+      slideCounter = -1;
+      animateSlide(slideImage);
     }
 
-    const goToPreviousSlide = () => {
-        if (slideCounter === 0) {
-            handleSlide(images.length)
-            slideCounter = images.length;
-        }
-
-        handleSlide(slideCounter)
-        slideCounter--;
-    }
-
-    const goToNextSlide = () => {
-        if (slideCounter === images.length - 1) {
-            startSlider()
-            slideCounter = -1;
-            animateSlide(slideImage)
-        }
-
-        slideImage.current.style.backgroundImage = `linear-gradient(
+    slideImage.current.style.backgroundImage = `linear-gradient(
           to right,
           rgba(34, 34, 34, 0.4),
           rgba(68, 68, 68, 0.4)
-          ),url(${images[slideCounter + 1].src})`;
-        slideText.current.innerHTML = images[slideCounter + 1].text;
-        slideCounter++;
-        animateSlide(slideImage)
-    }
+          ),url(${images[slideCounter + 1].picLink})`;
+    slideText.current.innerHTML = images[slideCounter + 1].name;
+    slideCounter++;
+    animateSlide(slideImage);
+  };
 
-    return { goToPreviousSlide, goToNextSlide }
-}
+  return { goToPreviousSlide, goToNextSlide };
+};
 
-export default useSlider
+export default useSlider;
